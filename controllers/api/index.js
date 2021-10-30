f31a1b6e9185f164fbff9d1211c0816cb0ec6923
Vince <starryblue7@gmail.com>
2021-10-30 15:30:38 -0700

    add blog and comment routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,8 +3,8 @@ const userRoutes = require('./userRoutes');
 const blogRoutes = require('./blogRoutes');
 const commentRoutes = require('./commentRoutes');
 
-// router.use('/users', userRoutes);
-// router.use('/blogs', blogRoutes);
-// router.use('/comments', commentRoutes);
+router.use('/users', userRoutes);
+router.use('/blogs', blogRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
