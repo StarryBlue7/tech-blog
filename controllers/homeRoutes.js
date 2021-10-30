e291f8a0835ceeb675d472221e3cb960261a5864
Vince <starryblue7@gmail.com>
2021-10-30 15:40:56 -0700

    add home routes behavior

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,19 +3,74 @@ const { User, Blog } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
- 
+    try {
+        const blogData = await Blog.findAll({
+        include: [
+            {
+            model: User,
+            attributes: ['name'],
+            },
+        ],
+        });
+
+        const blogs = blogData.map((blog) => blog.get({ plain: true }));
+
+        res.render('homepage', { 
+            blogs, 
+            logged_in: req.session.logged_in 
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 router.get('/blog/:id', async (req, res) => {
- 
+    try {
+        const blogData = await Blog.findByPk(req.params.id, {
+        include: [
+            {
+                model: User,
+                attributes: ['name'],
+            },
+        ],
+        });
+
+        const blog = blogData.get({ plain: true });
+
+        res.render('blog', {
+            ...blog,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 router.get('/dashboard', withAuth, async (req, res) => {
- 
+    try {
+        const userData = await User.findByPk(req.session.user_id, {
+            attributes: { exclude: ['password'] },
+            include: [{ model: Blog }],
+        });
+
+        const user = userData.get({ plain: true });
+
+        res.render('dashboard', {
+            ...user,
+            logged_in: true
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 router.get('/login', (req, res) => {
- 
+    if (req.session.logged_in) {
+        res.redirect('/dashboard');
+        return;
+    }
+
+    res.render('login');
 });
 
 module.exports = router;
