4b6da4fcfbbba7f9cccc959af76f53a9b164eb36
Vince <starryblue7@gmail.com>
2021-10-31 17:39:20 -0700

    add login/logout/account creation functionality

---
+++ public/js/login.js
@@ -0,0 +1,57 @@
+async function loginFormHandler() {
+    console.log('login')
+
+    const username = document.querySelector('#username-input').value.trim();
+    const password = document.querySelector('#password-input').value.trim();
+
+    if (username && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({ name: username, password: password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert('Login failed. Try again!');
+        }
+    }
+};
+
+async function signupFormHandler() {
+    console.log('signup')
+
+    const username = document.querySelector('#username-input').value.trim();
+    const password = document.querySelector('#password-input').value.trim();
+
+    if (username && password) {
+        const response = await fetch('/api/users', {
+            method: 'POST',
+            body: JSON.stringify({ name: username, password: password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert('Account creation failed. Try again!');
+        }
+    }
+};
+
+function checkAccount(event) {
+  event.preventDefault();
+  const currentAccount = event.submitter.value;
+  console.log(currentAccount)
+
+  if (currentAccount === "false") {
+    signupFormHandler();
+  } else {
+    loginFormHandler()
+  }
+}
+
+document
+  .querySelector('#account-form')
+  .addEventListener('submit', checkAccount);
